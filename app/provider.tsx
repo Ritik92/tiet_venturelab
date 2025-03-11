98258d194d18a955641c1042cb9ecaa84adbc092
Ritik92 <kartik292004@gmail.com>
2025-03-12 00:01:30 +0530

    useSearchParams Fix

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,12 +1,14 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { SessionProvider } from 'next-auth/react';
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
      
-
+        <Suspense>
         {children}
+        </Suspense>
+        
   
     </SessionProvider>
   );
